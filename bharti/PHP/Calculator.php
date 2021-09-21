0db7b81bcc824f0b191b572c4426813b6405fd00
Bhartibss <bharti.fcs@gmail.com>
2021-09-21 18:57:50 +0530

    issset

---
+++ bharti/PHP/Calculator.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<html>
+<head>
+	<title>Calcultor</title>
+</head>
+<div margin-top="800px">
+	<label>Calculator </label>
+<table border="1px" align="center"   width=50%>
+	<form>
+	<tr>
+		<th><label>Number1</label> </th>
+		<td> <input type="number" name="number" width=50%>
+			</td>
+	</tr>
+	<tr>
+		<th><label>Number2</label> </th>
+		<td> <input type="number" name="number2">
+			</td>
+	</tr>
+</form>
+</table>
+</div>
+</html>
