a4ed290fdbd23cc22bca3f3cf0b355846d6c8029
shreetuition <info@shreetuitioncenter.com>
2021-10-12 10:20:02 +0530

    Done

--- crud_php/constant.php
+++ crud_php/constant.php
@@ -1,9 +1,7 @@
 <?php 
-
 	define('MAIN_WEBSITE', 'http://localhost/php_11th_aug/crud_php/');
 	define('SERVER_NAME', 'localhost');
 	define('USER_NAME', 'root');
 	define('PASSWORD', '');
-	define('DB_NAME', 'northwind');
-	
+	define('DB_NAME', 'php_11_aug');	
 ?>
--- crud_php/create.php
+++ crud_php/create.php
@@ -19,6 +19,8 @@
      
     $name = $_POST['name'];
     $email = $_POST['email'];
+
+
     $city = $_POST['city'];
 
     if (isset($_POST['gender'])) {
@@ -43,7 +45,106 @@
         $className = "is-valid";
     }
 
-    $query = "INSERT INTO `employee`(`name`, `gender`, `hobby`, `email`, `city`, `dob`) VALUES ('$name', '$gender', '$hobby', '$email', '$city', '$dob')";
+
+
+    $profilePic = $_FILES['profilePic'];
+    $size_kb = round($profilePic['size']/1024);
+
+    // echo "<pre>";
+
+    // print_r($profilePic);
+
+    // die;
+
+    $ext = pathinfo($profilePic['name'], PATHINFO_EXTENSION);
+
+    if($ext =='jpg' OR $ext == 'png' OR $ext == 'PNG' OR $ext == 'JPG') {
+      $fileExt = true;
+    } else {
+      $fileExt = false;
+    }
+
+    // echo $fileExt;
+
+    // die;
+
+    if (file_exists("uploads/".$profilePic['name'])) {
+      $errorFile = "<span style='color:red;'>File is already there</span>";
+      $isStatusF = false;
+    } elseif($fileExt==false) {
+      $errorFile = "<span style='color:red;'>Invalid File Type</span>";
+      $isStatusF = false;
+    } elseif($size_kb<50 OR $size_kb >500) {
+      if ($size_kb<50) {
+        $errorFile = "<span style='color:red;'>File Size is too less</span>";
+        $isStatusF = false;
+      } else {
+        $errorFile = "<span style='color:red;'>File Size is too large</span>";
+        $isStatusF = false;
+      }
+      
+    } else {
+      
+        move_uploaded_file($profilePic['tmp_name'], "uploads/".$profilePic['name']);
+        $isStatusF = true;
+
+    }
+
+    // echo $errorFile;
+
+    // echo $isStatus;
+
+
+
+
+    //  if (!file_exists("uploads/".$profilePic['name'])) {
+    //    echo "We can upload the file";
+    //  } else {
+    //   echo "File is already there";
+    //  }
+
+    
+
+    // if ($size_kb>=50 && $size_kb <=500) {
+    //   echo "Ok";
+    // } else {
+    //   echo "File Size is too large";
+    // }
+
+    // if ($ext=='jpg' OR $ext == 'png' OR $ext == 'PNG' OR $ext == 'JPG') {
+    //   echo "Good to go";
+    // } else {
+    //   echo "Invalid file Type";
+    // }
+
+    // echo "<pre>";
+
+    // print_r($profilePic);
+
+    //die;
+
+
+    $queryEmail = "SELECT * FROM employee WHERE email = '$email'";
+
+    $resultE = mysqli_query($conn, $queryEmail);
+
+    if ($resultE->num_rows>0) {
+      
+      $emailMessage = "<span style='color:red;'>Email is already exits</span>";
+      $isStatusE = false;
+
+    } else {
+
+      $isStatusE = true;
+    }
+    // echo "<pre>";
+
+    // print_r($resultE);
+
+    // die;
+
+    if ($isStatusF && $isStatusE) {
+       $query = "INSERT INTO `employee`(`name`, `gender`, `hobby`, `email`, `city`, `dob`, `profilePic`) VALUES ('$name', '$gender', '$hobby', '$email', '$city', '$dob', '".$profilePic['name']."')";
 
     $result = mysqli_query($conn, $query);
    
@@ -55,6 +156,12 @@
     } else {
         $message = array("message" => "Something Error", "class" => "alert-danger") ;
     }
+    }
+
+    //die;
+
+   
+  
   }
 
 ?>
@@ -84,7 +191,7 @@
     }
   ?>
 
-  <form method="post">
+  <form method="post" enctype="multipart/form-data">
     <div class="form-group">
       <label for="email">Name:</label>  
       <input type="text" class="form-control form-control-sm <?php echo (isset($className)) ? $className : $className ?>"  placeholder="Enter name" name="name">
@@ -92,6 +199,11 @@
     <div class="form-group">
       <label for="pwd">Email:</label>
       <input type="email" class="form-control form-control-sm"  placeholder="Enter email" name="email">
+      <?php 
+
+        echo (isset($emailMessage)) ? $emailMessage : '';
+
+      ?>
     </div>
     <div class="form-group">
         <label>City</label>
@@ -143,6 +255,15 @@
     <div class="form-group">
       <label for="pwd">DOB:</label>
       <input type="date" class="form-control form-control-sm"  name="dob">
+    </div>
+
+    <div class="form-group">
+      <label>Profile Pic</label>
+      <input type="file" name="profilePic" class="form-control-file">
+
+      <?php 
+        echo (isset($errorFile)) ? $errorFile : '';
+      ?>
     </div>
     
     <button type="submit" name="submit" class="btn btn-primary">Submit</button>
