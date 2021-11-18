877dcad7f3e731f28decfa3ab7f1547f9e3180e5
shreetuition <info@shreetuitioncenter.com>
2021-11-18 12:27:46 +0530

    Done

--- JavaScript/custom.js
+++ JavaScript/custom.js
@@ -159,17 +159,216 @@ var lastName = "Kumar";
 
 // console.log(para.style.border = "1px solid blue");
 
-let getDiv = document.getElementById("Result");
+// let getDiv = document.getElementById("Result");
 
-getDiv.innerHTML = "<h1>This is Kumar</h1>";
-getDiv.style.color = "blue";
-getDiv.style.backgroundColor = "black";
+// getDiv.innerHTML = "<h1>This is Kumar</h1>";
+// getDiv.style.color = "blue";
+// getDiv.style.backgroundColor = "black";
 
 
-getDiv.style.cssText = `
-  color: red; 
-  background-color: black;
-  font-size: 50px;
-`;
+// getDiv.style.cssText = `
+//   color: red; 
+//   background-color: black;
+//   font-size: 50px;
+// `;
 
-console.log(getDiv);
+// console.log(getDiv);
+
+// var div = document.getElementById("para");
+
+// console.log(div.innerHTML);
+
+// var input = document.getElementById("name");
+
+// console.log(input.value);
+
+// var h1 = document.getElementsByTagName("h1");
+
+// console.log(h1[0].innerHTML = input.value);
+
+// console.log(div.style.backgroundColor = "red");
+// var onBulbImg = document.getElementById("bulb");
+// function onBulb() {
+	
+// 	onBulbImg.src = "img/ONbulb.jpg";
+// }
+
+// function offBulb() {
+// 	onBulbImg.src = "img/OFFbulb.jpg";
+// }
+
+// function onOffBulb() {
+// 	//console.log(onBulbImg.src);
+
+// 	if(onBulbImg.src=='http://localhost/php_11th_aug/javascript/img/ONbulb.jpg') {
+// 		onBulbImg.src = "img/OFFbulb.jpg";
+// 	} else {
+// 		onBulbImg.src='img/ONbulb.jpg';
+// 	}
+// } 
+
+// if (true) {}
+
+// if() {
+
+// } else {
+
+// }
+
+// if (true) {} else if {} else {}
+
+// switch() {
+
+// }
+
+// for loop 
+// while loop 
+// do while loop
+
+
+// var x = 5
+// var y = 10
+
+// if(x>y) {
+// 	console.log(x);
+// }
+
+// if() {
+
+// } else {
+
+// }
+
+
+// const person = {fname:"John", lname:"Doe", age:25};
+
+// let text = "";
+
+// for (let x in person) {
+
+//   text += person[x];
+
+//   //console.log(person[x]);
+
+//   //console.log(x);
+
+// }
+
+// console.log(text);
+
+
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = "";
+// for (let x of cars) {
+//   text += x;
+
+// //  console.log(x);
+// }
+
+// console.log(text);
+
+// {0: jan, 1 : frb}
+
+
+let dateObject = new Date();
+
+
+// console.log(dateObject.getHours());
+
+// console.log(dateObject.getMinutes());
+// console.log(dateObject.getSeconds());
+
+// console.log(dateObject.getHours()+":"+dateObject.getMinutes()+":"+dateObject.getSeconds());
+
+// document.getElementById("time").innerHTML = dateObject.getHours()+":"+dateObject.getMinutes()+":"+dateObject.getSeconds();
+
+// console.log(dateObject);
+
+console.log(dateObject.getFullYear()); // 2021
+
+
+console.log(dateObject.getMonth());
+
+console.log(dateObject.getDay());
+
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// var dateString = "2021/11/14";
+
+var dateString = "2021-11-14";
+var d = new Date(2010, 7, 5);
+var dayName = days[d.getDay()];
+
+// console.log(days[dateObject.getDay()]);
+
+console.log(dayName);
+
+
+
+// function GetFormattedDate() {
+//     var todayTime = new Date();
+//     console.log(todayTime);
+//     var month = format(todayTime .getMonth() + 1);
+//     var day = format(todayTime .getDate());
+//     var year = format(todayTime .getFullYear());
+//     return month + "/" + day + "/" + year;
+// }
+
+// GetFormattedDate()
+
+function geneateDate() {
+	var getCurrentDate = "11/18/2021"; // M-d-y
+
+	console.log(getCurrentDate); 
+
+
+	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+	var d = new Date(getCurrentDate);
+	var dayName = days[d.getDay()];
+
+	console.log(d);
+}
+
+
+
+function getRealTime() {
+	let dateObject = new Date();
+
+	let ourHours;
+	let ourMinutes;
+	let ourSeconds;
+	if((dateObject.getHours()).toString().length==1) {
+		
+		ourHours = "0"+(dateObject.getHours()).toString()
+		
+	} else {
+		ourHours = (dateObject.getHours()).toString();
+	}
+
+	if((dateObject.getMinutes()).toString().length==1) {
+		
+		ourMinutes = "0"+(dateObject.getMinutes()).toString()
+		
+	} else {
+		ourMinutes = (dateObject.getMinutes()).toString()
+	}
+
+	if((dateObject.getSeconds()).toString().length==1) {
+		
+		ourSeconds = "0"+(dateObject.getSeconds()).toString()
+		
+	} else {
+		ourSeconds = (dateObject.getSeconds()).toString()
+	}
+
+	document.getElementById("time").innerHTML = ourHours+":"+ourMinutes+":"+ourSeconds;
+}
+
+getRealTime()
+
+
+setInterval(
+	getRealTime, 1000
+);
--- JavaScript/index.php
+++ JavaScript/index.php
@@ -18,6 +18,22 @@
 
 	<p id="para">This is Para</p>
 
+	<input type="date" name="date" id="date">
+
+	<button type="button" onclick="geneateDate()">Report</button>
+
+	<h1 id="time"></h1>
+
+	<!-- <input type="text" name="name" id="name" value="Sharvan"> -->
+<!-- 	<br><br>
+	<img src="img/OFFbulb.jpg" id="bulb">
+	<br><br>
+
+	<button type="button" onclick="onBulb()">ON</button>
+	<button type="button" onclick="offBulb()">OFF</button>
+
+	<button type="button" onclick="onOffBulb()">ON/OFF</button> -->
+
 	<!-- <button type="button" onclick="">Test</button> -->
 
 	<!-- <button type="button" onclick="cofirmDelete()">Delete</button> -->
